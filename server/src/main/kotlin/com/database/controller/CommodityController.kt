817b57f9d47e84cf033827dc72da1e004d1d82e4
徐自华 <1525482627@qq.com>
2020-12-22 22:50:37 +0800

    基本实现

--- server/src/main/kotlin/com/database/controller/CommodityController.kt
+++ server/src/main/kotlin/com/database/controller/CommodityController.kt
@@ -17,38 +17,39 @@ class CommodityController {
 
     //所有商品列表
     @GetMapping("/commodityList")
-    fun commodityList():List<Commodity>{
+    fun commodityList(): List<Commodity> {
         return commodityDAO.findAll()
     }
 
     //单个商品详细信息
     @GetMapping("/commodity")
-    fun commodity(id:Int): Commodity {
+    fun commodity(id: Int): Commodity {
         return commodityDAO.findById(id).get()
     }
 
     //上架
     @PostMapping("/sell")
-    fun sell(@RequestBody commodity:Commodity): ResponseEntity<HttpStatus> {
+    fun sell(@RequestBody commodity: Commodity): ResponseEntity<HttpStatus> {
         commodityDAO.save(commodity)
         return ResponseEntity(HttpStatus.OK)
     }
 
     //个人出售的商品
     @GetMapping("/sellOfUser")
-    fun sellOfUser(serialNumber:String): List<Commodity> {
+    fun sellOfUser(serialNumber: String): List<Commodity> {
         return commodityDAO.findAllBySeller(serialNumber)
     }
 
     //个人购买的商品
     @GetMapping("/buyOfUser")
-    fun buyOfUser(serialNumber:String): List<Commodity> {
+    fun buyOfUser(serialNumber: String): List<Commodity> {
         return commodityDAO.findAllByBuyer(serialNumber)
     }
 
+    //获取交易记录（买家不为“”，但不是null）
     @GetMapping("/allTrade")
-    fun allTrade(empty:String): List<Commodity> {
-        return commodityDAO.findAllByBuyerIsNot(empty)
+    fun allTrade(): List<Commodity> {
+        return commodityDAO.findAllByBuyerIsNot("")
     }
 
 }
--- server/src/main/kotlin/com/database/model/CommodityDAO.kt
+++ server/src/main/kotlin/com/database/model/CommodityDAO.kt
@@ -2,8 +2,8 @@ package com.database.model
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CommodityDAO: JpaRepository<Commodity,Int> {
-    fun findAllBySeller(seller:String):List<Commodity>
-    fun findAllByBuyer(seller:String):List<Commodity>
-    fun findAllByBuyerIsNot(empty:String):List<Commodity>
+interface CommodityDAO : JpaRepository<Commodity, Int> {
+    fun findAllBySeller(seller: String): List<Commodity>
+    fun findAllByBuyer(seller: String): List<Commodity>
+    fun findAllByBuyerIsNot(empty: String): List<Commodity>
 }
