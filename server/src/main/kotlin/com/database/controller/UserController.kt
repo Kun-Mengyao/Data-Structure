6a01756fa9080b43e3ec0f1ca070542ef39d9a3b
徐自华 <1525482627@qq.com>
2020-12-25 22:42:54 +0800

    12.25 22:42

--- server/src/main/kotlin/com/database/controller/UserController.kt
+++ server/src/main/kotlin/com/database/controller/UserController.kt
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
+@CrossOrigin
 @RestController
 class UserController {
     @Autowired
@@ -64,27 +65,19 @@ class UserController {
 
     //修改个人信息,不包括修改密码
     @PostMapping("/modifyUserInfo")
-    fun modifyUserInfo(@RequestBody user: User): ResponseEntity<HttpStatus> {
+    fun modifyUserInfo(@RequestBody user: User): ResponseEntity<User> {
         //user.id = userDAO.findBySerialNumber(user.serialNumber)?.id!!
-
-        userDAO.save(user)
-
-        return ResponseEntity(HttpStatus.OK)
+        return ResponseEntity(userDAO.save(user), HttpStatus.OK)
     }
 
     //修改密码,需要确认两次
     @PostMapping("/modifyUserPassword")
     fun modifyUserPassword(
-        serialNumber: String, oldPassword: String, firstPassward: String, secondPassword: String
+        serialNumber: String, newPassword: String
     ): ResponseEntity<*> {
-        var user = userDAO.findBySerialNumber(serialNumber);
-        if (user!!.password != oldPassword) {
-            return ResponseEntity("用户旧密码输入不正确", HttpStatus.BAD_REQUEST)
-        }
-        if (firstPassward != secondPassword) {
-            return ResponseEntity("两次输入的密码不一致", HttpStatus.BAD_REQUEST)
-        }
-        user.password = firstPassward
+        val user = userDAO.findBySerialNumber(serialNumber);
+
+        user!!.password = newPassword
         userDAO.save(user)
         return ResponseEntity(user, HttpStatus.OK)
     }
